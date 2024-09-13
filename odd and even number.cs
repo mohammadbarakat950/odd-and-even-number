5fad79962cf62101b226e35f2915b99924d0f327
mohammadbarakat950 <40238983@hruc.ac.uk>
2024-09-13 10:43:52 +0100

    Add files via upload
    I have created the even and odd number identifier

---
+++ odd and even number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class patternRecognitionexample
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6 };
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number}is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number}is odd.");
+            }
+        }
+    }
+}
